35e7148df661c33b0b36c78279e5ab3ba09a0aa7
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-05-06 01:49:27 +0500

    Login Signup hooks implemented

--- app/src/main/java/com/example/socialmediaapp/MainActivity.java
+++ app/src/main/java/com/example/socialmediaapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.socialmediaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView email, pass;
+    Button loginbtn;
+    TextView activity_changer;
+
+    private void init(){
+        email=findViewById(R.id.etemail);
+        pass=findViewById(R.id.etpass);
+        loginbtn=findViewById(R.id.loginbtn);
+        activity_changer=findViewById(R.id.tvsignup);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2=new Intent(MainActivity.this, MainConfigurator.class);
+                startActivity(i2);
+
+            }
+        });
+
+        activity_changer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, Signup.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/socialmediaapp/Signup.java
+++ app/src/main/java/com/example/socialmediaapp/Signup.java
@@ -1,6 +1,11 @@
 package com.example.socialmediaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Signup extends AppCompatActivity {
+    EditText name, email, pass;
+    TextView actchanger;
+    Button signupbtn;
 
+    private void init(){
+        name=findViewById(R.id.etsignupname);
+        email=findViewById(R.id.etsignupemail);
+        pass=findViewById(R.id.etsignuppass);
+        actchanger=findViewById(R.id.tvlogin);
+        signupbtn=findViewById(R.id.signupbtn);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +34,22 @@ public class Signup extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        init();
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        actchanger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Signup.this, MainActivity.class);
+                startActivity(i);
+            }
         });
     }
 }
