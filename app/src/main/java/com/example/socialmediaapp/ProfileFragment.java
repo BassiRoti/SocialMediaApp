e22ec1f2c35fae0eec6071436ce62c7879734cd9
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-05-11 04:41:19 +0500

    Tried Profile fragment (done 90%). Has a bug

--- app/src/main/java/com/example/socialmediaapp/ProfileFragment.java
+++ app/src/main/java/com/example/socialmediaapp/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.socialmediaapp;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -16,15 +17,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 
@@ -37,17 +41,34 @@ public class ProfileFragment extends Fragment {
     SharedPreferences sp;
 
     Button logoutbtn;
+    Button editbtn;
+    SharedPreferences.Editor editor;
+    ProfileRVAdapter adapter;
+
+
 
     private void init(){
         userimg=v.findViewById(R.id.idLogo);
         username=v.findViewById(R.id.profilename);
         profilerv=v.findViewById(R.id.profilerv);
         logoutbtn=v.findViewById(R.id.logoutbtn);
+        editbtn=v.findViewById(R.id.editpfbtn);
         bio=v.findViewById(R.id.bio);
         sp= getContext().getSharedPreferences("authentication_data",MODE_PRIVATE);
-        SharedPreferences.Editor editor=sp.edit();
+        editor=sp.edit();
         username.setText(sp.getString("user_name",""));
         bio.setText(sp.getString("user_bio",""));
+        String id=FirebaseAuth.getInstance().getUid();
+        Query query = FirebaseDatabase.getInstance()
+                .getReference("posts")
+                .orderByChild("userID")
+                .equalTo(id);
+        FirebaseRecyclerOptions<PostsData> opt= new FirebaseRecyclerOptions
+                .Builder<PostsData>().
+                setQuery(query, PostsData.class).
+                build();
+        adapter=new ProfileRVAdapter(opt,requireContext(),id);
+        profilerv.setAdapter(adapter);
 
     }
 
@@ -80,12 +101,72 @@ public class ProfileFragment extends Fragment {
         logoutbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                editor.putString("Username","");
+                editor.putString("Bio","");
+                editor.putString("userID","");
+                editor.putString("Image","");
+                editor.apply();
                 FirebaseAuth.getInstance().signOut();
                 Intent i=new Intent(getContext(),MainActivity.class);
                 startActivity(i);
             }
         });
 
+        editbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                View vv = LayoutInflater.from(requireContext()).inflate(R.layout.custom_dialogue, null, false);
+                builder.setView(vv);
 
+                Button dialoguesavebtn = vv.findViewById(R.id.dialoguesavebtn);
+                EditText Dialoguebio = vv.findViewById(R.id.dialoguebio);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                dialoguesavebtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String updatedBio = Dialoguebio.getText().toString().trim();
+                        if (!updatedBio.isEmpty()) {
+                            FirebaseDatabase.getInstance()
+                                    .getReference("users")
+                                    .child(sp.getString("user_id", ""))
+                                    .child("Bio")
+                                    .setValue(updatedBio)
+                                    .addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            bio.setText(updatedBio);
+                                            editor.putString("user_bio", updatedBio).apply();
+                                            dialog.dismiss();
+                                            Toast.makeText(requireContext(), "Bio updated successfully", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(requireContext(), "Failed to update bio", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(requireContext(), "Bio cannot be empty", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 }
---
+++ app/src/main/java/com/example/socialmediaapp/ProfileRVAdapter.java
@@ -0,0 +1,88 @@
+package com.example.socialmediaapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileRVAdapter extends FirebaseRecyclerAdapter<PostsData, ProfileRVAdapter.ViewHolder> {
+
+    Context c;
+    String uid;
+
+    public ProfileRVAdapter(@NonNull FirebaseRecyclerOptions<PostsData> options, Context context, String id) {
+        super(options);
+        c=context;
+        uid=id;
+    }
+
+
+    @Override
+    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull PostsData model) {
+        String tempid= model.getUserID();
+            FirebaseDatabase.getInstance().getReference("users").child(tempid).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        String tempusername=snapshot.child("Username").getValue(String.class);
+                        holder.username.setText(tempusername);
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(c, "an error occured while fetching username", Toast.LENGTH_SHORT).show();
+                }
+            });
+            holder.caption.setText(model.getCaption());
+            holder.timespam.setText(model.getTimespam());
+
+            MultiImagePagerAdapter adapter=new MultiImagePagerAdapter(c,model.getImageUrls());
+            Log.d("ImageDebug", "Image URLs: " + model.getImageUrls());
+
+            holder.vp2.setAdapter(adapter);
+
+
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v=LayoutInflater.from(c).inflate(R.layout.custom_feed_view,parent,false);
+        return new ViewHolder(v);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView img;
+        TextView username, caption, timespam;
+        ViewPager2 vp2;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img=itemView.findViewById(R.id.tvuserimg);
+            username=itemView.findViewById(R.id.usernameText);
+            caption=itemView.findViewById(R.id.tvcaption);
+            timespam=itemView.findViewById(R.id.tvtimespam);
+            vp2=itemView.findViewById(R.id.vpimage);
+
+        }
+    }
+}
